1356f6fda5f649045d411fabe6284645de89e070
문경림 <naru@takeone.co.kr>
2025-10-15 18:20:27 +0900

    update chapter5

---
+++ chapter5/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function Header() 
+{
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  )
+}
+
+
+function App() {
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
