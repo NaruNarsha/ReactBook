3c3b1e7147c4e75dd766267df4ddaeecf27dc872
문경림 <naru@takeone.co.kr>
2025-10-21 20:17:37 +0900

    update chapter5

--- chapter5/src/App.js
+++ chapter5/src/App.js
@@ -4,6 +4,10 @@ import Body from './component/Body';
 import Footer from './component/Footer';
 
 
+function ChildComp(){
+  return <div>child component</div>
+}
+
 function App() {
 
   const BodyProps = {
@@ -15,7 +19,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Body {...BodyProps} />
+      <Body>
+        <ChildComp />
+      </Body>
       <Footer />
     </div>
   );
--- chapter5/src/component/Body.js
+++ chapter5/src/component/Body.js
@@ -1,18 +1,69 @@
 import "./Body.css";
 
-function Body(props) {
-    
-    const {name, location, fovoriteList} = props;
-    console.log(name, location);
+// function Body( {name, location, favorList = []}) {
+
+//     console.log(name, location, favorList);
+
+//     return (
+//         <div className = "body">
+//             {name}님, 안녕하세요! {location}에 오신 것을 환영합니다.
+
+//             <br />
+//                 {favorList.length}개의 좋아하는 취미가 있습니다.
+//         </div>
+//     );
+// }
+
+
+// Body.defaultProps = {
+//     favorList: [],
+// };
+
+//==============================================================================
+
+// function Body({children}) {
+//     console.clear();
+//     console.log(children);
+//     return <div className = "body">{children}</div>;
+// }
+
+
+//==============================================================================
+
+
+// function Body(){
+//     function handleOnClick(){
+//         alert("버튼이 클릭되었습니다.");
+//     }
+
+//     return (
+//         <div className="body">
+//             <button onClick = {handleOnClick}>클릭하세요</button>
+//         </div>
+//     )
+// }
+
+//==============================================================================
+
+function Body(){
+    function handleOnClick(e){
+        console.clear();
+        console.log(e);
+        console.log(e.target.name)
+    }
 
     return (
         <div className = "body">
-            {name}님, 안녕하세요! {location}에 오신 것을 환영합니다.
-        
-            <br />
-                {fovoriteList.length}개의 좋아하는 취미가 있습니다.
+            <button name="A버튼" onClick={handleOnClick}>
+                A버튼
+            </button>
+            <button name="B버튼" onClick={handleOnClick}>
+                B버튼
+            </button>
         </div>
     );
 }
+
+
 
 export default Body;
