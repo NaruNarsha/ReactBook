7473dcb0f65e7d99e22e2d58364767e82007d3d1
문경림 <naru@takeone.co.kr>
2025-10-27 19:18:26 +0900

    update project02

---
+++ project02/src/component/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return  <div className ="Header">
+                <h3>오늘은 😊</h3>
+                <h1>{new Date().toDateString()}</h1>
+            </div>;
+}
+
+export default Header;
