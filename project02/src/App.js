91e82c456a464cca90bcc49b98e7ac0aeffd68c0
문경림 <naru@takeone.co.kr>
2025-10-29 21:17:48 +0900

    update project02

--- project02/src/App.js
+++ project02/src/App.js
@@ -1,13 +1,53 @@
 import './App.css';
+import { useState } from "react";
 import Header from "./component/Header";
-import ToolEditor from "./component/TodoEditor";
+import TodoEditor from "./component/TodoEditor";
 import TodoList from "./component/TodoList";
 
+
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime()
+  },
+    {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime()
+  },
+    {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createdDate: new Date().getTime()
+  },
+];
+
+
 function App() {
+
+  const [todo, setTodo] = useState(mockTodo);
+
+  function onCreate(content){
+    const newItem = {
+      id:0,
+      content,
+      isDone: false,
+      createdDate: new Date().getTime(),
+    };
+
+    setTodo([newItem, ...todo]);
+  };
+
+
+
   return (
     <div className="App">
       <Header />
-      <ToolEditor />
+      <TodoEditor />
       <TodoList />
     </div>
     );
